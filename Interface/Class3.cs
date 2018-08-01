8ce296cdbe4b348089c74c1a011d90dec6bed728
Darron Charles <darrchar@gmail.com>
2018-08-01 16:04:11 -0400

    Committ

---
+++ Interface/Class3.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Practice
+{
+    class Animal
+    {
+        private string name;
+        private string sound;
+
+        public const string SHELTER = "Derek's Home for Animals";
+        //set at run-time inside consructors, can't be changed
+        public readonly int idNum;
+        
+        public void MakeSound()
+        {
+            Console.WriteLine("{0} says {1}", name, sound);
+        }
+        public Animal()
+            : this("No Name", "No Sound") { } // :this calls the main constructor
+        public Animal(string name)
+            : this(name, "No Sound") { }
+        public Animal(string name, string sound)
+        {
+            SetName(name);
+            Sound = sound;
+
+            NumOfAnimals = 1;
+
+            Random rnd = new Random();
+            idNum = rnd.Next(1, 123123450);
+        }
+        public void SetName(string name)
+        {
+            if (!name.Any(char.IsDigit))
+            {
+                this.name = name;
+            }
+            else
+            {
+                this.name = "No Name";
+                Console.WriteLine("Name can't contain numbers");
+            }
+        }
+        //Getter without using properties
+        public string GetName()
+        {
+            return name;
+        }
+        //Getter by using properties
+        public string Sound
+        {
+            get { return sound; }
+            set
+            {
+                if(value.Length > 10)
+                {
+                    sound = "No Sound";
+                    Console.WriteLine("Sound is too long");
+                }
+                else
+                {
+                    sound = value;
+
+                }
+            }
+        }
+
+        public string Owner { get; set; } = "No Owner"; //default field
+
+        public static int numOfAnimals = 0;
+
+        public static int NumOfAnimals
+        {
+            get { return numOfAnimals; }
+            set { numOfAnimals += value; }
+        }
+           
+        
+    }
+}
---
+++ Interface/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //public private, protected Constants, Read-Only, Constructors, Setters, Getters, Properties and Static Fields
+            Console.WriteLine("Class 6");
+            Animal cat = new Animal();
+            cat.SetName("Whiskers");
+            cat.Sound = "Meow";
+
+            Console.WriteLine("The cat is named {0} and says {1}", cat.GetName(), cat.Sound);
+            cat.Owner = "Derek";
+
+            Console.WriteLine("{0} owner is {1}", cat.GetName(), cat.Owner); 
+            Console.WriteLine("{0} shelter id is {1}", cat.GetName(), cat.idNum);
+            Console.WriteLine("# of Animals : {0}", Animal.NumOfAnimals); //static var belong to class
+
+            Animal fox = new Animal("Oscar", "Whatthefoxsaaaaay");
+            Console.WriteLine("# of Animals : {0}", Animal.NumOfAnimals);
+
+
+
+            Console.ReadKey(true);
+
+            Console.WriteLine("Class 10");
+            ElectronicsAndDrive.Vehicle buick = new ElectronicsAndDrive.Vehicle("Buick", 4, 160);
+
+            if (buick is ElectronicsAndDrive.IDrivable)
+            {
+                buick.Move();
+                buick.Stop();
+            }
+            else
+            {
+                Console.WriteLine("The {0} can't be driven", buick.Brand);
+            }
+            IElectronicDevice TV = TVRemote.GetDevice();
+
+            PowerButton powBut = new PowerButton(TV);
+            powBut.Execute();
+            powBut.Undo();
+
+            Console.ReadLine();
+
+            //Protected, Inheritance, Delegates, Super Classes, Polymorphism, Virtual, Override, Inner Classes 
+            Console.WriteLine("Class 7");
+            Console.ReadLine();
+        }
+
+        
+    }
+}
