afcf83ac675bdf55991a861750f9af2a3ca8b001
Darron Charles <darrchar@gmail.com>
2018-08-06 22:51:05 -0400

    rect

--- Interface/Shape.cs
+++ Interface/Shape.cs
@@ -11,5 +11,48 @@ namespace Practice
         {
             Console.WriteLine($"This is a {Name}");
         }
+        public abstract double Area();
+    }
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Name = "Circle";
+            Radius = radius;
+        }
+
+        public override double Area()
+        {
+            return Math.PI * Math.Pow(Radius, 2.0);
+        }
+        public override void getInfo()
+        {
+            base.getInfo();
+            Console.WriteLine($"It has a Radius of: {Radius}");
+        }
+    }
+    class Rectangle : Shape
+    {
+        public double Length { get; set; }
+        public double Width { get; set; }
+
+        public Rectangle(double length, double width)
+        {
+            Name = "Rectangle"; 
+            Length = length;
+            Width = width;
+        }
+
+        public override double Area()
+        {
+            return Length * Width;
+        }
+        public override void getInfo()
+        {
+            base.getInfo();
+            Console.WriteLine($"It has a Length of {Length} and a Width of {Width}");
+        }
     }
 }
